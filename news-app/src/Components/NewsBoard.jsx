3af110f8194b4ea0db333fc27ccc7e88d9643555
Amitha Pai <amitha.pai@zapcg.com>
2024-02-21 17:38:25 +0530

    News app using news API

---
+++ news-app/src/Components/NewsBoard.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem';
+
+const NewsBoard = ({category}) => {
+
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=6307b35ecc1f470f8b3ab53e8329cf34`;
+        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
+    },[category])
+
+  return (
+    <div>
+      <h2 className='text-center'>Latest <span className='badge bg-danger'>News</span></h2>
+      {articles.map((news,index)=> {
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} ul={news.url} />
+      })}
+    </div>
+  )
+}
+
+export default NewsBoard
