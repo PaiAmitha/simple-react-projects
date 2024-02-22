a72bbf834e2dfb64c3a5347c6c4af1da2db4524d
Amitha Pai <amitha.pai@zapcg.com>
2024-02-22 12:20:38 +0530

    Contains errors

--- expense-tracker/src/Components/TransactionList.jsx
+++ expense-tracker/src/Components/TransactionList.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { GlobalContext } from '../Context/GlobalState';
+import { Transaction } from './Transaction';
 
 const TransactionList = () => {
+
+    const { transactions} = useContext(GlobalContext);
+
   return (
     <div>
        <h3>History</h3>
-      <ul className="list">
-         <li className="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li> 
+       <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
     </div>
   )
